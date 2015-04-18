be23ee170ff1b960a7f08d0649c318c56e073e80
Jennifer Be <jbe@berkeley.edu>
2015-04-18 13:42:45 -0700

    migrated plants and animals

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418203429) do
+ActiveRecord::Schema.define(version: 20150418203805) do
+
+  create_table "animals", force: true do |t|
+    t.string   "name"
+    t.integer  "birthTime"
+    t.integer  "maturation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "farmers", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "plants", force: true do |t|
+    t.string   "name"
+    t.integer  "birthTime"
+    t.integer  "maturation"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
