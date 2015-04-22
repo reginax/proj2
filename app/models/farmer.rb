ae7451658a7b31b0a325986e3b33f6d702c731e7
Richard Qian <richard.c.qian@berkeley.edu>
2015-04-21 17:42:16 -0700

    added devise values

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,4 +1,6 @@
 class Farmer < ActiveRecord::Base
+	devise :database_authenticatable, :registerable, 
+		   :recoverable, :rememberable, :trackable, :validatable
 	has_many :plants
 	has_many :animals
 end
