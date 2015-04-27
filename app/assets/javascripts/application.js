db555fea288cd2bb7c6af5fe459269a5c8235e9f
Jimmy Lee <jimlee604@gmail.com>
2015-04-26 23:01:07 -0700

    gritter works kind of

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require turbolinks
 //= require foundation
 //= require_tree .
+//= require gritter
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-  	flash[:notice] = "todo, change css for gritter"
+  	# flash[:notice] = "todo, change css for gritter"
+    # gflash :success => "The product has been created successfully!"
     spaceless_plant = Plant.all
     @plant = spaceless_plant.sample
     # @plant.space_id = 1
