e95e38adcbc1e5e64cf171b22b3b58a15f73fa4e
Jimmy Lee <jimlee604@gmail.com>
2015-04-29 22:35:58 -0700

    cheese

--- backupproj2/app/models/farmer.rb
+++
@@ -1,6 +0,0 @@
-class Farmer < ActiveRecord::Base
-	devise :database_authenticatable, :registerable, 
-		   :recoverable, :rememberable, :trackable, :validatable
-	has_many :plants
-	has_many :animals
-end
--- backupproj2/app/models/plant.rb
+++
@@ -1,5 +0,0 @@
-class Plant < ActiveRecord::Base
-  @@matdict = {"Potato" => 20, "Squash" => 40, "Wintermelon" => 50, "Corn" => 60, "Strawberry" => 55, "Fries" => 20}
- 
-	belongs_to :farmer
-end
